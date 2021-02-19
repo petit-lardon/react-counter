daffdcccb10b1ebc0a12a79aeb3428f693833cb6
Julien Lavie <jlavie@leadformance.com>
2021-02-19 17:13:29 +0100

    create simple counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import Counters from "./components/Counters";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      {id: 1, value: 1},
+      {id: 2, value: 2},
+      {id: 3, value: 3}
+    ]
+  }
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+
+    this.setState({counters});
+  }
+
+  handleDelete = (counterId) => {
+    console.log(counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({counters});
+  }
+
+  handleIncrement = (counter) => {
+    console.log(counter);
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = {...counter};
+    counters[index].value++;
+    this.setState({counters});
+  }
+
+  render() {
+    return (
+        <React.Fragment>
+          <Navbar totalCounters={this.state.counters.filter(c => c.value > 0).length} />
+          <main className="container">
+            <Counters
+                counters={this.state.counters}
+                onReset={this.handleReset}
+                onDelete={this.handleDelete}
+                onIncrement={this.handleIncrement} />
+          </main>
+        </React.Fragment>
+    );
+  }
 }
 
 export default App;
